67c65db00602e042218275fbed9d48c19cc108d5
jayasurya231 <123251024+jayasurya231@users.noreply.github.com>
2023-07-29 18:52:14 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import GreetingCard from './GreetingCard';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Welcome to My App!</h1>
+      <GreetingCard title="Hello, Friend!" message="Hope you're having a great day." />
+    </div>
+  );
+};
+
+export default App;
